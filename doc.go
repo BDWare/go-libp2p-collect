c7356948da250b0abb796102189c9556dafa7bcb
Hu Yingcong <me@huyc.info>
2020-04-17 01:25:14 +0800

    updage doc

--- doc.go
+++ doc.go
@@ -1,3 +1,3 @@
-// The collect package provides facilities for the Publish/Subscribe/Collect pattern of
+// Package collect provides facilities for the Publish/Subscribe/Collect pattern of
 // message propagation and data collections, useful for distributed query.
 package collect
