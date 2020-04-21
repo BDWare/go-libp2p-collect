012846c1346e35a197035428580626c2d2be2a0e
Hu Yingcong <me@huyc.info>
2020-04-22 01:09:32 +0800

    update tests

--- relay_test.go
+++ relay_test.go
@@ -277,6 +277,79 @@ func TestRelayLeaveAndJoin(t *testing.T) {
 	}
 }
 
+func TestRequestContextExpired(t *testing.T) {
+	// FinalResponseHandler shouldn't be called after the request context expires.
+	mnet := mock.NewMockNet()
+	pubhost, err := mnet.NewLinkedPeer()
+	assert.NoError(t, err)
+	subhost, err := mnet.NewLinkedPeer()
+	assert.NoError(t, err)
+
+	pub, err := NewRelayPubSubCollector(pubhost)
+	assert.NoError(t, err)
+	sub, err := NewRelayPubSubCollector(subhost)
+	assert.NoError(t, err)
+	mnet.ConnectPeers(pubhost.ID(), subhost.ID())
+
+	// time to connect
+	time.Sleep(50 * time.Millisecond)
+
+	topic := "test-topic"
+	payload := []byte{1, 2, 3}
+
+	cctx, cc := context.WithCancel(context.Background())
+	// handlePub and handleSub is both request handle,
+	// but handleSub will send back the response
+	handlePub := func(ctx context.Context, r *pb.Request) *pb.Intermediate {
+		out := &pb.Intermediate{
+			Sendback: false,
+			Payload:  payload,
+		}
+		return out
+	}
+	handleSub := func(ctx context.Context, r *pb.Request) *pb.Intermediate {
+		// cancel the request context
+		cc()
+		// wait for cancellation
+		time.Sleep(50 * time.Millisecond)
+
+		out := &pb.Intermediate{
+			Sendback: true,
+			Payload:  payload,
+		}
+		return out
+	}
+
+	err = pub.Join(topic, WithRequestHandler(handlePub))
+	assert.NoError(t, err)
+	err = sub.Join(topic, WithRequestHandler(handleSub))
+	assert.NoError(t, err)
+
+	// time to join
+	time.Sleep(50 * time.Millisecond)
+
+	notif := func(ctx context.Context, rp *pb.Response) {
+		assert.Fail(t, "unexpected callation of finalRespHandler")
+	}
+
+	err = pub.Publish(
+		topic,
+		payload,
+		WithFinalRespHandler(notif),
+		WithRequestContext(cctx))
+	assert.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond)
+
+}
+
+func TestFinalDeduplication(t *testing.T) {
+	// A -- B
+	// B returns 2 response to A, A drop the second one.
+}
+
+// tuple nodes' tests
+
 func TestDeduplication(t *testing.T) {
 	// A -- B -- C
 	// A broadcasts, then B and C response to it with the same content;
@@ -289,7 +362,7 @@ func TestNoRequestIDForResponse(t *testing.T) {
 	// A broadcasts a request R, and B should know R's route;
 	// what will happen if R expired in B,
 	// and at the same time C send R's response to B?
-	// We have some strategy to skip the forgetful node fault.
+	// We have some strategy to skip the forgetful node.
 	// for example, we can forward this response to a random node.
 	// But now, we just drop it.
 }
