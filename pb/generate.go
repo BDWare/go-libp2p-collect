a2c5f90d28db3d4dfe9a2fc9b30200f28d094b8f
Hu Yingcong <me@huyc.info>
2020-11-26 22:31:26 +0800

    delete overlay

--- pb/generate.go
+++ pb/generate.go
@@ -2,4 +2,3 @@ package pb
 
 //go:generate go get -u github.com/gogo/protobuf/protoc-gen-gogofaster
 //go:generate protoc -I. --gogofaster_out=paths=source_relative:. pubsubcollect.proto
-//go:generate protoc -I. --gogofaster_out=paths=source_relative:. overlay.proto
