b1d656cb004ae5a1eb0afbd2d66179dea6ef2c2d
Hu Yingcong <me@huyc.info>
2021-04-09 15:18:20 +0800

    add local response test

--- intbfs_test.go
+++ intbfs_test.go
@@ -191,3 +191,52 @@ func TestCollectorSendRecv(t *testing.T) {
 	case <-okch:
 	}
 }
+
+func TestLocalResponse(t *testing.T) {
+	mnet := mock.NewMockNet()
+	pubhost, err := mnet.NewLinkedPeer()
+	assert.NoError(t, err)
+
+	pub, err := NewIntBFSCollector(pubhost)
+	assert.NoError(t, err)
+
+	// time to connect
+	time.Sleep(50 * time.Millisecond)
+
+	topic := "test-topic"
+	payload := []byte{1, 2, 3}
+
+	// handlePub and handleSub is both request handle,
+	// but handleSub will send back the response
+	handlePub := func(ctx context.Context, r *Request) *Intermediate {
+		assert.Equal(t, payload, r.Payload)
+		assert.Equal(t, pubhost.ID(), r.Control.Requester)
+		out := &Intermediate{
+			Hit:     true,
+			Payload: payload,
+		}
+		return out
+	}
+
+	err = pub.Join(topic, WithRequestHandler(handlePub))
+	assert.NoError(t, err)
+
+	// time to join
+	time.Sleep(50 * time.Millisecond)
+
+	okch := make(chan struct{})
+	notif := func(ctx context.Context, rp *Response) {
+		assert.Equal(t, payload, rp.Payload)
+		okch <- struct{}{}
+	}
+	err = pub.Publish(topic, payload, WithFinalRespHandler(notif))
+	assert.NoError(t, err)
+
+	// after 2 seconds, test will failed
+	select {
+	case <-time.After(2 * time.Second):
+		assert.Fail(t, "we don't receive enough response in 2s")
+	case <-okch:
+	}
+
+}
